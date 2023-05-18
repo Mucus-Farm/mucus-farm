0bd35107ef9514333ff3efa6622db5727a499e31
0xNahhh <0xNahhh@gmail.com>
2023-05-18 18:43:02 +0900

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-preprocessor"
+import fs from "fs";
+import "@nomicfoundation/hardhat-toolbox";
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
+  paths: {
+    sources: "./src",
+    cache: "./cache_hardhat",
+  },
+};
+
+export default config;
